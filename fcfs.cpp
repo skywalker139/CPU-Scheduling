b72e776383fca016ea60179ee7db2e67797bfd3c
skywalker139 <akshatnamdeo2412@gmail.com>
2024-06-11 04:32:35 +0530

    fcfs algo added

--- fcfs.cpp
+++ fcfs.cpp
@@ -4,6 +4,7 @@ struct process{
     int pid;
     int arrival_time;
     int burst_time;
+    int priority;
     int starting_time;
     int waiting_time;
     int response_time;
@@ -30,18 +31,7 @@ void gantt_chart(vector<process> processes_set){
     }
 }
 
-// void sort_process_set(vector<process> &processes_set){
-//     for(auto* it : &processes_set){
-//         if(it->arrival_time>(it+1)->arrival_time){
-//             swap(*it,*(it+1))
-//         }
-//         if(it->arrival_time=(it+1)->arrival_time){
-//             if(it->pid>(it+1)->pid){
-//                 swap(*it,*(it+1));
-//             }
-//         }
-//     }
-// }
+
 void sort_process_set(vector<process> &processes_set) {
     for (size_t i = 0; i < processes_set.size(); ++i) {
         for (size_t j = i + 1; j < processes_set.size(); ++j) {
@@ -53,10 +43,11 @@ void sort_process_set(vector<process> &processes_set) {
     }
 }
 
-
 vector<process> fcfs(vector<process> processes_set){
     sort_process_set(processes_set);
     int time=0;
+    int total_waiting_time=0, total_turnaround_time=0;
+    
     for(auto process: processes_set){ //process = elements of p from main, process=p1 p2 p3...
         if(time<=process.arrival_time){
             //implies cpu is waiting for next task which might arrive after time+x time later, time+x will be arrival time of next process, eg. t=3, next arrival time=4, t=4=starting time
@@ -70,18 +61,26 @@ vector<process> fcfs(vector<process> processes_set){
         process.completion_time = process.arrival_time + process.burst_time;
         process.waiting_time = process.starting_time - process.arrival_time;
         process.turnaround_time = process.completion_time - process.starting_time;
-        time=time+process.burst_time;
+        time = time + process.burst_time;
+        
+        total_waiting_time += process.waiting_time;
+        total_turnaround_time += process.turnaround_time;
     }
+    float average_waiting_time = total_waiting_time/(processes_set.size());
+    float average_turnaround_time = total_turnaround_time/(processes_set.size());
 }
 
+vector<process> sjf(vector<process> processes_set){
+    //shortest job first algo, tree structure
 
+}
 
 int main(){
     vector<process> process_set{
-        {1, 0, 8, 1, 0, 0, 0, 0},
-        {2, 1, 4, 2, 0, 0, 0, 0},
-        {3, 2, 9, 3, 0, 0, 0, 0},
-        {4, 3, 5, 4, 0, 0, 0, 0}
+        {1, 0, 8, 1, 0, 0, 0, 0, 0},
+        {2, 1, 4, 2, 0, 0, 0, 0, 0},
+        {3, 2, 9, 3, 0, 0, 0, 0, 0},
+        {4, 3, 5, 4, 0, 0, 0, 0, 0}
     };
     sort_process_set(process_set);
     gantt_chart(process_set);
